e445ffa599db5cdd46b23b9b0bcc8c17d4279652
Origamiboss <vincentclimer@gmail.com>
2026-01-26 15:33:25 -0600

    Final redesign

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import "./StyleSheets/colors.css";
+
 import Layout from './Pages/Layout';
 import Home from './Pages/Home';
 import About from './Pages/About';
@@ -7,7 +9,7 @@ import Book from './Pages/Book';
 import Cart from './Pages/Cart';
 import Checkout from './Pages/Checkout';
 import Books from './Pages/Books';
-import Calendar from './Pages/Calendar';
+import Newsletter from './Pages/Newsletter';
 import EventPage from './Pages/EventPage';
 
 function App() {
@@ -20,7 +22,7 @@ function App() {
                 <Route path="/cart" element={<Cart />} />
                 <Route path="/checkout" element={<Checkout />} />
                 <Route path="/books" element={<Books />} />
-                <Route path="/newsletter" element={<Calendar />} />
+                <Route path="/newsletter" element={<Newsletter />} />
                 <Route path="/events/:id" element={<EventPage />} />
                 <Route path="*" element={<h2>Page Not Found</h2>} />
             </Routes>
